d1f459cc520b7a7dbf29dd2bf19b1af58d57df81
Bine Stancic <stancic.bine@outlook.com>
2019-09-25 13:41:29 +0100

    First Commit

---
+++ script.js
@@ -0,0 +1,9 @@
+const percentageTip = document.QuerySelector(['data-percentage'])
+const totalBill = document.QuerySelector(['data-total-bill'])
+const isGo = document.QuerySelector(['data-isgo'])
+
+const finalBill = document.QuerySelector(['data-bill-final'])
+
+
+
+console.log(totalBill)
